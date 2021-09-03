ce5089f56d10252749aa5169c19e539f2fce1bb2
Reon0429-cat <oonishireon110@gmail.com>
2021-09-03 22:14:08 +0900

    first commit

---
+++ Graph-Practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Graph-Practice
+//
+//  Created by 大西玲音 on 2021/09/03.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+
+
+}
